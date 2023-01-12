2f327ddf1d7aa74d32d2090643a8a79193e44cb2
Vaidehi-Kokare <vaidehikokare@gmail.com>
2023-01-12 11:27:30 +0530

    Added: Rolling dice to get number between 1 and 6

--- src/main/java/com/bridgelabz/snakeladder/SnakeAndLadder.java
+++ src/main/java/com/bridgelabz/snakeladder/SnakeAndLadder.java
@@ -1,10 +1,14 @@
 package com.bridgelabz.snakeladder;
 public class SnakeAndLadder {
     static int pos = 0;
-
+    static int rollDice(){
+        return (int) (Math.random()*10 % 6 + 1);
+    }
     public static void main(String[] args) {
         System.out.println("Welcome to Snake and Ladders Game");
         System.out.println("Start position is: " + pos);
+        int dice = rollDice();
+        System.out.println(dice);
 
     }
 }
